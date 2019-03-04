1e9c476bbe212baf088b54ba66d4e7676eadc6c1
Jono Brain <jono@digitalblahblah.com>
2019-03-04 12:23:36 +0000

    Added Chrome code

--- server.js
+++ server.js
@@ -2,7 +2,8 @@
 var prerender = require('./lib');
 
 var server = prerender({
-  chromeLocation: '/usr/bin/google-chrome-stable'
+  chromeLocation: '/usr/bin/google-chrome-stable',
+  chromeFlags: ['--no-sandbox', '--headless', '--disable-gpu', '--remote-debugging-port=9222', '--hide-scrollbars']
 });
 
 server.use(prerender.whitelist());
